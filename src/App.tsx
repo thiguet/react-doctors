20b7eec1f153654ae416b2463e9422979523b86d
Thiguet <thiguet2014@gmail.com>
2021-02-15 09:58:02 -0300

    feat: added app DOM renderer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { getData } from './DOMScanner/doctorsTable';
+import { getFilterData } from './DOMScanner/filter';
+import { DoctorsRows } from './components/DoctorRow/DoctorRow';
+import mergeDoctorData from './services/Init/mergeDoctorData';
 
-export const App: React.FC = () => {
-    return <div>asdasdasd</div>;
-};
+// HTML DOM Scan -> Gets data from the HTML received.
+const selectOptions = getFilterData();
+const doctorsData = getData();
+
+mergeDoctorData();
+
+// API conciliate -> Merge DOM and API data.
+
+// ReactDOM.render(React.createElement(SearchInput), document.getElementById('searchContainer'));
+// ReactDOM.render(React.createElement(Select, { options }), document.getElementById('searchContainer'));
+ReactDOM.render(React.createElement(DoctorsRows, { rows: doctorsData }), document.getElementById('doctors'));
