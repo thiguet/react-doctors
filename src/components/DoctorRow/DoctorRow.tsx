47fcd363ed1c93c29687f8bd57f88b8f7274a2de
Thiguet <thiguet2014@gmail.com>
2021-02-15 10:22:04 -0300

    feat: added colors to doctors availability

--- src/components/DoctorRow/DoctorRow.tsx
+++ src/components/DoctorRow/DoctorRow.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { AvailabilityText, Doctor } from '../../types';
 import './DoctorRow.css';
 
+const getAvailabilityClass = (isAvailable: Boolean) => (isAvailable ? 'available' : 'unavailable');
+
+const getAvailabilityText = (isAvailable: Boolean) =>
+    isAvailable ? AvailabilityText['Mark as Available'] : AvailabilityText['Mark as Unavailable'];
+
 export const DoctorsRow: React.FC<Doctor> = ({ id, name, zipCode, cityName, isAvailable }) => (
-    <tr className={isAvailable ? 'available' : 'unavailable'} data-upin={id}>
+    <tr className={getAvailabilityClass(isAvailable)} data-upin={id}>
         <td>{name}</td>
         <td>{zipCode}</td>
         <td>{cityName}</td>
         <td>
-            <button className={`button button-outline ${isAvailable ? 'available' : 'unavailable'}`}>
-                {isAvailable ? AvailabilityText['Mark as Available'] : AvailabilityText['Mark as Unavailable']}
+            <button className={`button button-outline ${getAvailabilityClass(isAvailable)}`}>
+                {getAvailabilityText(isAvailable)}
             </button>
         </td>
     </tr>
