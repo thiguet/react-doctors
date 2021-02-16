d35b44230d74dbebd075b1c3831eb043ea499276
Thiguet <thiguet2014@gmail.com>
2021-02-16 12:25:25 -0300

    feat: added doctor HTML DOM scanner

--- src/services/DOMScanner/doctorsTable.ts
+++ src/services/DOMScanner/doctorsTable.ts
@@ -1,24 +1,24 @@
-import { AvailabilityText, Doctor } from '../../types';
+import { AvailabilityText, Doctor } from "../../types";
 
-export const getData = () =>
-    [...document.querySelectorAll('tbody tr')].map(tr => {
-        const id = tr.getAttribute('data-upin');
-        const tdData = [...tr.children].map(child => child.innerHTML);
+export const getData = (): Doctor[] =>
+    [...document.querySelectorAll("tbody tr")].map((tr) => {
+        const id = tr.getAttribute("data-upin");
+        const tdData = [...tr.children].map((child) => child.innerHTML);
         const isAvailable = [...tr.children].pop()?.children[0].innerHTML;
 
-        tdData.pop(); // Remove isArray
+        tdData.pop(); // Remove isAvailable td innerHTML
 
-        return buildDoctor([id, ...tdData, isAvailable]);
+        return buildDoctor([id!, ...tdData!, isAvailable!]);
     });
 
-export const buildDoctor = (arr: Array<any>): Doctor => ({
-    id: arr[0],
+export const buildDoctor = (arr: Array<string>): Doctor => ({
+    id: +arr[0],
     name: arr[1],
     zipCode: +arr[2],
     cityName: arr[3],
-    isAvailable: isAvailable(arr[4]),
+    isAvailable: isAvailable(arr[4] as AvailabilityText),
 });
 
-const isAvailable = (str: AvailabilityText) => {
-    return AvailabilityText[str] === AvailabilityText['Mark as Available'];
+const isAvailable = (str: AvailabilityText): boolean => {
+    return AvailabilityText[str] === AvailabilityText["Mark as Unavailable"];
 };
