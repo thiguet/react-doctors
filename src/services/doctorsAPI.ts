4e87ac2d738a37c09ed94872f500840da747f75c
Thiguet <thiguet2014@gmail.com>
2021-02-16 12:25:55 -0300

    refactor: added return types

--- src/services/doctorsAPI.ts
+++ src/services/doctorsAPI.ts
@@ -1,17 +1,26 @@
-import axios from 'axios';
-import { DBDoctor } from '../types';
+import axios from "axios";
+import { DBDoctor } from "../types";
 
-const url = 'http://localhost:3030/doctors';
+const url = "http://localhost:3030/doctors";
 
 const instance = axios.create({
     baseURL: url,
     headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     },
 });
 
-export const getDoctors = async (): Promise<DBDoctor[]> => (await instance.get(url))?.data as DBDoctor[];
+export const getDoctors = async (): Promise<DBDoctor[]> =>
+    (await instance.get(url))?.data as DBDoctor[];
 
-export const updateDoctor = (data: DBDoctor) => instance.put(`${url}/${data.upin}`, data);
+export const updateDoctor = (data: DBDoctor): Promise<void> =>
+    instance.put(`${url}/${data.upin}`, data);
 
-export const updateDoctors = (arr: DBDoctor[]) => arr.map(updateDoctor);
+export const updateDoctorsAvailability = (
+    upin: number,
+    isAvailable: boolean
+): Promise<void> =>
+    instance.patch(`${url}/${upin}`, { available: isAvailable });
+
+export const updateDoctors = (arr: DBDoctor[]): Promise<void>[] =>
+    arr.map(updateDoctor);
