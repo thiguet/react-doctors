030096442d12e204e9223e93acb96d93d5da0325
Thiguet <thiguet2014@gmail.com>
2021-02-16 12:16:43 -0300

    feat: added axe and merge data fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { getData } from './services/DOMScanner/doctorsTable';
-import { getFilterData } from './services/DOMScanner/filter';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import mergeDoctorData from './services/Init/mergeDoctorData';
+import axe from "@axe-core/react";
 
-import HOCDoctorFilter from './components/DoctorFilter/HOCDoctorFilter';
-import DoctorRowsWithFilter from './components/DoctorRow/DoctorRowsWithFilter';
+import { getFilterData } from "./services/DOMScanner/filter";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import mergeDoctorData from "./services/Init/mergeDoctorData";
+
+import DoctorSelectFilter from "./components/DoctorSelectFilter/DoctorSelectFilter";
+import DoctorTextFilter from "./components/DoctorTextFilter/DoctorTextFilter";
+import DoctorRowsWithFilter from "./components/DoctorRow/DoctorRowsWithFilter";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 // HTML DOM Scan -> Gets data from the HTML received.
 const selectOptions = getFilterData();
-const doctorsData = getData();
+
+if (process.env.NODE_ENV !== "production") {
+    axe(React, ReactDOM, 1000);
+}
 
 // API conciliate -> Merge DOM and API data.
-mergeDoctorData();
-
-// ReactDOM.render(
-//     React.createElement(DoctorFilter, { options: selectOptions }),
-//     document.getElementById('searchContainer')
-// );
+if (localStorage.getItem("HAS_MERGED_DATA_BEFORE") !== "Y") {
+    mergeDoctorData(); // This merge and persist the HTML Data to the DB.
+    localStorage.setItem("HAS_MERGED_DATA_BEFORE", "Y");
+}
 
 const renderReactComponents = () => {
     ReactDOM.render(
         <Provider store={store}>
-            <HOCDoctorFilter options={selectOptions} />
+            <DoctorSelectFilter options={selectOptions} />
         </Provider>,
-        document.querySelectorAll('.column.column-20:nth-child(2)')[0]
+        document.querySelectorAll(".column.column-20:nth-child(2)")[0]
     );
     ReactDOM.render(
         <Provider store={store}>
             <DoctorRowsWithFilter />
         </Provider>,
-        document.getElementById('doctors')
+        document.getElementById("doctors")
+    );
+    ReactDOM.render(
+        <Provider store={store}>
+            <DoctorTextFilter />
+        </Provider>,
+        document.getElementById("searchContainer")
+    );
+    ReactDOM.render(
+        <h1>Doctors</h1>,
+        document.querySelectorAll(".row:nth-child(1)")[0]
     );
 };
 
