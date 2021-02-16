f12c6b7fb40028b55905916ce530057dfcff745b
Thiguet <thiguet2014@gmail.com>
2021-02-16 11:55:21 -0300

    chore: added jest workaround to styles and other files.

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,19 @@
-import type { Config } from '@jest/types';
+import type { Config } from "@jest/types";
 
 export default async (): Promise<Config.InitialOptions> => {
     return {
+        preset: "ts-jest",
+        testEnvironment: "jsdom",
         verbose: true,
-        testPathIgnorePatterns: ['/node_modules/'],
+        testPathIgnorePatterns: ["/node_modules/"],
+        roots: ["<rootDir>"],
+        moduleNameMapper: {
+            "^@/(.*)$": "<rootDir>/src/$1",
+        },
+        transform: {
+            ".(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+                "<rootDir>/jest-config/file-mock.js",
+            ".(css|less)$": "<rootDir>/jest-config/style-mock.js",
+        },
     };
 };
