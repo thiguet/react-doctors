5b32e277aa369acafbf16a9f6f0fb5a221fe941c
Thiguet <thiguet2014@gmail.com>
2021-02-16 12:10:48 -0300

    style: linting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,48 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    target: 'web',
-    mode: 'development',
-    entry: path.resolve(__dirname, './src/index.ts'),
+    target: "web",
+    mode: "development",
+    entry: path.resolve(__dirname, "./src/index.ts"),
     resolve: {
-        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+        alias: {
+            "#": path.resolve(__dirname, "tests/"),
+            "@": path.resolve(__dirname, "src/"),
+        },
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader'],
+                use: ["babel-loader"],
             },
             {
                 test: /\.(css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test: /\.(ts|tsx)$/,
-                loader: 'awesome-typescript-loader',
+                loader: "awesome-typescript-loader",
             },
         ],
     },
     output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js',
+        path: __dirname + "/dist",
+        publicPath: "/",
+        filename: "bundle.js",
     },
-    plugins: [new webpack.HotModuleReplacementPlugin(), new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+    ],
     devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
+        contentBase: path.resolve(__dirname, "./dist"),
         hot: true,
     },
 };
