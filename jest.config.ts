1904fb66c4b73085b81d344011845ee30407253b
Thiguet <thiguet2014@gmail.com>
2021-02-16 18:11:57 -0300

    chore: added mapping to tests folder

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,7 @@ export default async (): Promise<Config.InitialOptions> => {
         roots: ["<rootDir>"],
         moduleNameMapper: {
             "^@/(.*)$": "<rootDir>/src/$1",
+            "^#/(.*)$": "<rootDir>/tests/$1",
         },
         transform: {
             ".(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
